306b3b4f32e68c5e03513b34126ace495d2bd0c0
Mansoor Ali <mansoorsahito601@gmail.com>
2026-01-27 09:55:32 +0500

    React Interview Preparation task1

--- App.tsx
+++ App.tsx
@@ -1,45 +1,44 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  TouchableOpacity,
+} from 'react-native';
+import React, { useState } from 'react';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
+const App = () => {
+  const [value, setValue] = useState(0);
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setValue(value + 1)}
+      >
+        <Text style={styles.btntext}>Press Me</Text>
+      </TouchableOpacity>
+      <Text>{value}</Text>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  button: {
+    backgroundColor: '#DDDDDD',
+    height: 50,
+    width: 100,
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+  },
+  btntext: { color: 'blue', fontSize: 14, fontWeight: 'semibold' },
 });
-
-export default App;
