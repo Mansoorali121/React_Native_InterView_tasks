f7f40b02e581abb2a1ee316ab66dd6746a86de3a
Mansoor Ali <mansoorsahito601@gmail.com>
2026-01-29 11:56:14 +0500

    Data fetched with loading indicator

--- src/Screens/Task9RefreshApi.js
+++ src/Screens/Task9RefreshApi.js
@@ -1,14 +1,19 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Task9RefreshApi = () => {
   const [Dataa, setDataa] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const Fetchdata = async () => {
     try {
-      //   setLoading(true);
       const reponse = await axios.get(
         'https://69609023e7aa517cb79661a7.mockapi.io/Books',
       );
@@ -20,22 +25,34 @@ const Task9RefreshApi = () => {
       setLoading(false);
     }
   };
+  //   if (loading) return <ActivityIndicator size={'large'} color={'blue'} />;
+
   useEffect(() => {
     Fetchdata();
   }, []);
   return (
     <View style={styles.container}>
-      <FlatList
-        data={Dataa}
-        keyExtractor={item => item.id.toString()}
-        renderItem={({ item }) => {
-          return (
-            <View style={styles.itemStyle}>
-              <Text>{item.id}</Text>
+      {loading ? (
+        <View style={{marginTop:20}}>
+            <ActivityIndicator size="large" color="blue" />
             </View>
-          );
-        }}
-      />
+      ) : (
+        <FlatList
+          data={Dataa}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.itemStyle}>
+                <Text style={{ color: 'red' }}>{item.id}</Text>
+
+                <Text style={{ color: 'black' }}>{item.author}</Text>
+                <Text style={{ color: 'black' }}>{item.name_of_book}</Text>
+              </View>
+            );
+          }}
+        />
+      )}
     </View>
   );
 };
@@ -45,10 +62,12 @@ export default Task9RefreshApi;
 const styles = StyleSheet.create({
   container: { flex: 1 },
   itemStyle: {
-    padding: 20,
+    padding: 15,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
-
-    backgroundColor: '#632020dd',
+    marginTop: 10,
+    marginHorizontal: 20,
+    backgroundColor: '#DDDDDD',
+    marginBottom: 20,
   },
 });
