5d7ab63af0fbe7fdd8d34dfd14ec0c5442e5feff
Mansoor Ali <mansoorsahito601@gmail.com>
2026-01-29 10:24:34 +0500

    Task 8 Data Fetched Completed

--- src/Screens/Task8APIdataFetch.js
+++ src/Screens/Task8APIdataFetch.js
@@ -1,18 +1,63 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
-
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 const Task8APIdataFetch = () => {
+  const [Dataa, setDataa] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const FetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users',
+      );
+      const json = await response.json();
+      setDataa(json);
+      console.log('Fetched Data', json);
+      setLoading(false);
+    } catch (error) {
+      console.log('Error Fetching Data', error);
+      setLoading(false);
+    }
+  };
+  if (loading) {
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator size="large" color="black" />
+      </View>
+    );
+  }
 
-    
+  useEffect(() => {
+    FetchData();
+  }, []);
+
   return (
-    <View>
-      <Text>Task8APIdataFetch</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={Dataa}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => {
+          return <Text>{item.id}</Text>;
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 
 export default Task8APIdataFetch;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#fff', },
+  loader: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+});
