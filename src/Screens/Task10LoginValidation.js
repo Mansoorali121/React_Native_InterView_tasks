96e68662529fb328ab1800ae9b796575c7dbe14f
Mansoor Ali <mansoorsahito601@gmail.com>
2026-01-30 09:53:08 +0500

    Task10 Ui Complete

--- src/Screens/Task10LoginValidation.js
+++ src/Screens/Task10LoginValidation.js
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 
 const Task10LoginValidation = () => {
   return (
     <View style={styles.container}>
       <View style={styles.formcontainer}>
-        <View>
+        <View style={styles.inputcontainer}>
           <TextInput placeholder="Enter Name" style={styles.input} />
-          <TextInput placeholder="Enter Name"  style={styles.input}/>
-          <TextInput placeholder="Enter Name"  style={styles.input}/>
+          <TextInput placeholder="Enter Email" style={styles.input} />
+          <TextInput placeholder="Enter Password" style={styles.input} />
+          <Text style={styles.forgettext}>Forgot Password? </Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.registerbutton}>Register</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -27,5 +38,24 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     borderRadius: 20,
   },
-  input:{},
+  inputcontainer: { alignItems: 'center', justifyContent: 'center' },
+  input: {
+    borderColor: '#F3F3F3',
+    borderWidth: 1,
+    borderRadius: 5,
+    width: 317,
+    height: 55,
+    marginTop: 20,
+    color: '#ffff',
+  },
+  button: {
+    width: 317,
+    height: 55,
+    alignItems: 'center',
+    marginTop: 40,
+    backgroundColor: '#F83758',
+    justifyContent: 'center',
+  },
+  registerbutton: { color: '#ffff', borderRadius: 20, fontSize: 14 },
+  forgettext: { color: 'blue', marginLeft: 210, marginTop: 5 },
 });
